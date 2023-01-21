9fa9fba40726cc5f3ee0ca9da06e41c1c139e6d9
iSommie <anajembaedwin@gmail.com>
2023-01-21 13:11:43 +0100

    updated logo in component and campaigndetails

--- client/src/components/FundCard.jsx
+++ client/src/components/FundCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import { tagType, logo } from '../assets';
+import { tagType } from '../assets';
+import nikaLogo from '../assets/nikaLogo.gif';
 import { daysLeft } from '../utils';
 
 const FundCard = ({ owner, title, description, target, deadline, amountCollected, image, handleClick }) => {
@@ -34,7 +35,7 @@ const FundCard = ({ owner, title, description, target, deadline, amountCollected
 
         <div className="flex items-center mt-[20px] gap-[12px]">
           <div className="w-[30px] h-[30px] rounded-full flex justify-center items-center bg-[#13131a]">
-            <img src={logo} alt="user" className="w-1/2 h-1/2 object-contain"/>
+            <img src={nikaLogo} alt="user" className="w-1/2 h-1/2 object-contain"/>
           </div>
           <p className="flex-1 font-epilogue font-normal text-[12px] text-[#808191] truncate">by <span className="text-[#b2b3bd]">{owner}</span></p>
         </div>
